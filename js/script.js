e3c4a8b9b565a9c272bc7783754a7222f86db932
Andrea <andreanuzzo@hotmail.it>
2021-11-04 16:13:50 +0100

    completed pass creation with JS

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+// Chiedre all'utente il proprio Nome
+let firstName = prompt('Inserisci il tuo nome');
+console.log(firstName)
+
+// Chiedere all'utente il proprio Cognome
+let lastName = prompt('Inserisci il tuo Cognome');
+console.log(lastName)
+
+// Chiedere  all'utente il proprio colore preferito
+let color = prompt('Inserisci il tuo colore preferito');
+console.log(color)
+
+// Generare la password (nomecognomecolore21)
+const password = (`${firstName}${lastName}${color}21`);
+console.log(password)
