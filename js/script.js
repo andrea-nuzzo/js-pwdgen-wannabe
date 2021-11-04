616fb5a7f4cb0a02efdfc2d8de7a601650ca65f2
Andrea <andreanuzzo@hotmail.it>
2021-11-04 16:23:33 +0100

    commented the console logs

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 
 // Chiedre all'utente il proprio Nome
 let firstName = prompt('Inserisci il tuo nome');
-console.log(firstName)
+// console.log(firstName)
 
 // Chiedere all'utente il proprio Cognome
 let lastName = prompt('Inserisci il tuo Cognome');
-console.log(lastName)
+// console.log(lastName)
 
 // Chiedere  all'utente il proprio colore preferito
 let color = prompt('Inserisci il tuo colore preferito');
-console.log(color)
+// console.log(color)
 
 // Generare la password (nomecognomecolore21)
 const password = (`${firstName}${lastName}${color}21`);
-console.log(password)
+// console.log(password)
 
 
 //Inserisci la pass nel div html
